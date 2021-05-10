eefd53eae56bd331a429bfa79559d9f642dfa58c
A1Gard <a1gard@4xmen.ir>
2021-05-10 06:36:48 +0430

    complete matrial colors add project page info

--- src/assets/js/win_vars.js
+++ src/assets/js/win_vars.js
@@ -13,4 +13,99 @@ window.colors = [
         value: 'Colors.red',
         color: '#f44336'
     },
+    {
+        name: 'DeepOrange',
+        value: 'Colors.deepOrange',
+        color: '#ff5722'
+    },
+    {
+        name: 'Orange',
+        value: 'Colors.orange',
+        color: '#ff9800'
+    },
+    {
+        name: 'Amber',
+        value: 'Colors.amber',
+        color: '#ffc107'
+    },
+    {
+        name: 'Yellow',
+        value: 'Colors.yellow',
+        color: '#ffeb3b'
+    },
+    {
+        name: 'Lime',
+        value: 'Colors.lime',
+        color: '#cddc39'
+    },
+    {
+        name: 'LightGreen',
+        value: 'Colors.lightGreen',
+        color: '#8bc34a'
+    },
+    {
+        name: 'Green',
+        value: 'Colors.green',
+        color: '#4caf50'
+    },
+    {
+        name: 'Teal',
+        value: 'Colors.teal',
+        color: '#009688'
+    },
+    {
+        name: 'Cyan',
+        value: 'Colors.cyan',
+        color: '#00bcd4'
+    },
+    {
+        name: 'LightBlue',
+        value: 'Colors.lightBlue',
+        color: '#03a9f4'
+    },
+    {
+        name: 'Blue',
+        value: 'Colors.blue',
+        color: '#2196f3'
+    },
+    {
+        name: 'Indigo',
+        value: 'Colors.indigo',
+        color: '#3f51b5'
+    },
+    {
+        name: 'Purple',
+        value: 'Colors.purple',
+        color: '#9c27b0'
+    },
+    {
+        name: 'DeepPurple',
+        value: 'Colors.deepPurple',
+        color: '#673ab7'
+    },
+    {
+        name: 'BlueGrey',
+        value: 'Colors.blueGrey',
+        color: '#607d8b'
+    },
+    {
+        name: 'Brown',
+        value: 'Colors.brown',
+        color: '#795548'
+    },
+    {
+        name: 'Grey',
+        value: 'Colors.grey',
+        color: '#9e9e9e'
+    },
+    {
+        name: 'Black',
+        value: 'Colors.black',
+        color: '#000000'
+    },
+    {
+        name: 'White',
+        value: 'Colors.white',
+        color: '#ffffff'
+    },
 ];
