2a4e7b5916ca6b8f04bac921758c168658caaaaa
A1Gard <a1gard@4xmen.ir>
2021-05-13 06:56:40 +0430

    add save anubias project file

--- src/background.js
+++ src/background.js
@@ -1,113 +1,128 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import {app, protocol, BrowserWindow} from 'electron'
+import {createProtocol} from 'vue-cli-plugin-electron-builder/lib'
+import installExtension, {VUEJS_DEVTOOLS} from 'electron-devtools-installer'
 import path from 'path'
-import * as fs from "fs";
+import fs from "fs";
+
 const ipc = require('electron').ipcMain
 const dialog = require('electron').dialog
 const isDevelopment = process.env.NODE_ENV !== 'production';
-var win ;
+var win;
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
+    {scheme: 'app', privileges: {secure: true, standard: true}}
 ])
 
 async function createWindow() {
 
-  // Create the browser window.
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      // nodeIntegration: true,
-      // enableRemoteModule: true,
-      // contextIsolation: false ,
-      enableRemoteModule: false, // turn off remote
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      preload: path.join(__dirname, 'preload.js')
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+
+            // Use pluginOptions.nodeIntegration, leave this alone
+            // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+            // nodeIntegration: true,
+            // enableRemoteModule: true,
+            // contextIsolation: false ,
+            enableRemoteModule: false, // turn off remote
+            nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    win.removeMenu();
+    win.maximize();
+
+
+    if (process.env.WEBPACK_DEV_SERVER_URL) {
+        // Load the url of the dev server if in development mode
+        await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+        if (!process.env.IS_TEST) win.webContents.openDevTools()
+    } else {
+        createProtocol('app')
+        // Load the index.html when not in development
+        win.loadURL('app://./index.html');
     }
-  })
-  win.removeMenu();
-  win.maximize();
-
-
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
-  } else {
-    createProtocol('app')
-    // Load the index.html when not in development
-    win.loadURL('app://./index.html');
-  }
 }
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installExtension(VUEJS_DEVTOOLS)
-    } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
+    if (isDevelopment && !process.env.IS_TEST) {
+        // Install Vue Devtools
+        try {
+            await installExtension(VUEJS_DEVTOOLS)
+        } catch (e) {
+            console.error('Vue Devtools failed to install:', e.toString())
+        }
     }
-  }
-  createWindow()
+    createWindow()
 
 })
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', (data) => {
-      if (data === 'graceful-exit') {
-        app.quit()
-      }
-    })
-  } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
-  }
+    if (process.platform === 'win32') {
+        process.on('message', (data) => {
+            if (data === 'graceful-exit') {
+                app.quit()
+            }
+        })
+    } else {
+        process.on('SIGTERM', () => {
+            app.quit()
+        })
+    }
 }
 
 ipc.on('open-file-dialog', function (event) {
-  dialog.showOpenDialog({
-    properties: ['openFile']
-  }, function (files) {
-    if (files) event.sender.send('selected-file', files)
-  })
+    dialog.showOpenDialog({
+        properties: ['openFile']
+    }, function (files) {
+        if (files) event.sender.send('selected-file', files)
+    })
 })
-ipc.on('save-file-dialog', function (event,arg) {
-  dialog.showSaveDialog( arg.dialog ).then(function (data) {
-    // if(fileName === undefined) return
-    // event.sender.send('saved-file', fileName)
-    fs.writeFile( data.filePath, arg.data,function (err) {
-      win.webContents.send('saved-file','saved '+ data.filePath);
+ipc.on('save-file-dialog', function (event, arg) {
+    dialog.showSaveDialog(arg.dialog).then(function (data) {
+        // if(fileName === undefined) return
+        // event.sender.send('saved-file', fileName)
+        win.webContents.send('saved-file', 'try to save ' + data.filePath);
+        try {
+            let  filename = data.filePath.trim();
+            if (filename.substr(filename.length - 4) !== '.anb'){
+                filename += '.anb';
+            }
+            fs.writeFile(filename, JSON.stringify(arg.data), function (err) {
+                if (err) {
+                    win.webContents.send('saved-file', 'error ' + err.message);
+                }
+                win.webContents.send('saved-file', 'saved : ' + data.filePath);
+            });
+        } catch (e) {
+            win.webContents.send('saved-file', 'error ' + e.message);
+
+        }
+
     });
-  });
 
 })
