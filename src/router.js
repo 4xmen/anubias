9209db1e934f7342c38d2d629ef3d7e4219c2eb6
A1Gard <a1gard@4xmen.ir>
2021-05-23 09:20:58 +0430

    fix production bug black screen

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import Router from "vue-router";
 
 Vue.use(Router);
 export default new Router({
-    mode: "history",
+    // mode: "history",
     base: process.env.BASE_URL,
     routes: [
         {
