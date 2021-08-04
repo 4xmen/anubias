6b3c8ea3145fba8d6e7e5dc445ba836010d71946
A1Gard <a1gard@4xmen.ir>
2021-08-04 15:18:34 +0430

    fix build info for os [tested on Linux only]

--- vue.config.js
+++ vue.config.js
@@ -3,42 +3,41 @@ module.exports = {
         electronBuilder: {
             preload: './src/preload.js',
             // Or, for multiple preload files:
-            // "appId": "app.anubias.desktop",
-            // "productName": "Anubias",
-            // "copyright": "Copyright © 2021 ${author}",
-            // // "asar": false,
-            // "mac": {
-            //     "target": "dmg",
-            //     "icon": "./public/icon.icns",
-            //     "category": "public.app-category.developer-tools"
-            // },
-            // "win": {
-            //     "target": [
-            //         "nsis",
-            //         "msi"
-            //     ],
-            //     "icon": "./public/256x256.png"
-            // },
-            // "linux": {
-            //     // "category": "Development",
-            //     "target": "AppImage",
-            //     "desktop": "Anubias"
-            // },
-            // "directories": {
-            //     "buildResources": "public",
-            // },
-            // // "publish": null,
-            // "fileAssociations": [
-            //     {
-            //         "ext": "anb",
-            //         "name": "Anbuias project",
-            //         "role": "Anubias",
-            //         "perMachine": true,
-            //         "description": "Anubias project file to develop native app anywhere painless"
-            //     }
-            // ],
             builderOptions: {
-                extraResources: [
+                "appId": "app.anubias.desktop",
+                "productName": "Anubias",
+                "copyright": "Copyright © 2021 ${author}",
+                "mac": {
+                    "target": "dmg",
+                    "icon": "./public/icon.icns",
+                    "category": "public.app-category.developer-tools"
+                },
+                "win": {
+                    "target": [
+                        "msi",
+                        "portable"
+                    ],
+                    "icon": "./public/256x256.png"
+                },
+                "linux": {
+                    "category": "Development",
+                    "target": "AppImage",
+                    "desktop": "./public/anubias.desktop"
+                },
+                "directories": {
+                    "buildResources": "public",
+                },
+                // "publish": null,
+                "fileAssociations": [
+                    {
+                        "ext": "anb",
+                        "name": "Anbuias project",
+                        "role": "None",
+                        "mimeType": "application/json",
+                        "description": "Anubias project file to develop native app anywhere painless"
+                    }
+                ],
+                "extraResources": [
                     {
                         "from": "resources",
                         "to": "resources",
