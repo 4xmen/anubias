9ec1c39c76a3fc200a0e17c02211f1b230a6aebb
A1Gard <a1gard@4xmen.ir>
2023-03-28 20:40:53 +0330

    added material icon added spinner to preloader

--- anubias2/src/css.ts
+++ anubias2/src/css.ts
@@ -1,5 +1,6 @@
 import 'vazirmatn/Vazirmatn-Variable-font-face.css';
-import 'remixicon/fonts/remixicon.css'
+import 'remixicon/fonts/remixicon.css';
+import 'material-icons/css/material-icons.css';
 
 import "./style.css";
 export {};
