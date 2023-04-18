e89dbf5542b7795724dca8978f047a28ab915478
A1Gard <a1gard@4xmen.ir>
2023-04-18 10:21:16 +0330

    added ignore more than one app bar fixed appbar position

--- anubias2/src/stores/projectStore.js
+++ anubias2/src/stores/projectStore.js
@@ -3,8 +3,11 @@ import ide from './ideStore';
 import Store from 'electron-store';
 import {ipcRenderer} from 'electron';
 import {getUniqueName} from "../ide/js/component-control";
+import {useToast} from "vue-toastification";
 
 const storage = new Store();
+const toast = useToast();
+
 /***
  * project store to store all data about project
  * like: project info, components
@@ -36,58 +39,77 @@ const projectStore = {
             this.dispatch('setIdeTitle', title);
             storage.set('lastLoadedProject', project);
             this.dispatch('ide/setActivePage', project.entryPoint);
-            ipcRenderer.send('set-has-project',true);
+            ipcRenderer.send('set-has-project', true);
         },
         ADD_COMPONENT_TO_PAGE(state, {pageIndex, isVisual, component}) {
             // console.log;
             let c = {...component};
-            c.name = getUniqueName(state.project.pages[pageIndex],component.name);
-            if (isVisual){
-                state.project.pages[pageIndex].children.visual.push(c);
-            }else{
-                state.project.pages[pageIndex].children.nonVisual.push(c);
+            // check is app app bar
+            if (c.type === 'appbar') {
+                c.name = getUniqueName(state.project.pages[pageIndex], component.name);
+                if (c.name === 'appbar2') {
+                    // show warning
+                    let msg = `You can't add ${c.type} more!`;
+                    toast.warning(msg);
+                }else
+                    {
+                        state.project.pages[pageIndex].children.visual.unshift(c);
+                    }
+                } else {
+                    // usual components
+                    c.name = getUniqueName(state.project.pages[pageIndex], component.name);
+                    if (isVisual) {
+                        state.project.pages[pageIndex].children.visual.push(c);
+                    } else {
+                        state.project.pages[pageIndex].children.nonVisual.push(c);
+                    }
+                }
+                this.dispatch('ide/setMenuCanUndo', true);
             }
-            this.dispatch('ide/setMenuCanUndo',true);
+        ,
         },
-    },
-    actions: {
-        /**
-         *
-         * @param context
-         * @param project : Object anubias project object
-         */
-        createProject(context, project) {
-            context.commit('CREATE_PROJECT', project);
-        },
-        /**
-         *
-         * @param context
-         * @param project : Object anubias project object
-         */
-        loadProject(context, project) {
-            context.commit('LOAD_PROJECT', project);
-        },
-        /**
-         * add component to page
-         * @param context
-         * @param pageIndex : Number
-         * @param isVisual : Boolean
-         * @param component : Object anubias component object
-         */
-        addComponentToPage(context, {pageIndex, isVisual, component}) {
-            // const currentPageIndex = this.
-            context.commit('ADD_COMPONENT_TO_PAGE',{ pageIndex: pageIndex, isVisual: isVisual, component: component });
-        },
+        actions: {
+            /**
+             *
+             * @param context
+             * @param project : Object anubias project object
+             */
+            createProject(context, project) {
+                context.commit('CREATE_PROJECT', project);
+            },
+            /**
+             *
+             * @param context
+             * @param project : Object anubias project object
+             */
+            loadProject(context, project) {
+                context.commit('LOAD_PROJECT', project);
+            },
+            /**
+             * add component to page
+             * @param context
+             * @param pageIndex : Number
+             * @param isVisual : Boolean
+             * @param component : Object anubias component object
+             */
+            addComponentToPage(context, {pageIndex, isVisual, component}) {
+                // const currentPageIndex = this.
+                context.commit('ADD_COMPONENT_TO_PAGE', {
+                    pageIndex: pageIndex,
+                    isVisual: isVisual,
+                    component: component
+                });
+            },
 
-    },
-    getters: {
-        getPage: (state) => (i) => {
-            return state.project.pages[i];
         },
-        appColor: (state) =>{
-            return state.project.appColor;
+        getters: {
+            getPage: (state) => (i) => {
+                return state.project.pages[i];
+            },
+            appColor: (state) => {
+                return state.project.appColor;
+            }
         }
-    }
-};
+    };
 
-export default projectStore;
+    export default projectStore;
