912a55ca030a1adc2c9cfa38491119c1e939de6a
A1Gard <a1gard@4xmen.ir>
2023-05-20 04:37:39 +0330

    fixed add action bug added icons list into properties add hr style

--- anubias2/src/ide/components/modern-properties.vue
+++ anubias2/src/ide/components/modern-properties.vue
@@ -10,47 +10,51 @@
         Name is invalid...
       </div>
     </collapsible>
-    <collapsible v-if="properties.align !== undefined" :title="'Align ('+showValue(properties.align)+')'" icon="ri-layout-right-2-line">
+    <collapsible v-if="properties.align !== undefined" :title="'Align ('+showValue(properties.align)+')'"
+                 icon="ri-layout-right-2-line">
       <div class="btn-group">
-        <div  v-for="(v,i) in extractItems(properties.validator.align.regex)"
-              :class="'btn '+(v === properties.align?'active':'')"
-              :key="i">
-            <div v-if="v === 'null'" title="default" @click="properties.align = 'null'">
-               <i class="ri-eraser-line"></i>
-            </div>
-            <div v-else :title="v" @click="properties.align = v">
-                <i :class="`ri-align-${v}`"></i>
-            </div>
+        <div v-for="(v,i) in extractItems(properties.validator.align.regex)"
+             :class="'btn '+(v === properties.align?'active':'')"
+             :key="i">
+          <div v-if="v === 'null'" title="default" @click="properties.align = 'null'">
+            <i class="ri-eraser-line"></i>
+          </div>
+          <div v-else :title="v" @click="properties.align = v">
+            <i :class="`ri-align-${v}`"></i>
+          </div>
         </div>
       </div>
     </collapsible>
     <collapsible title="Text properties" icon="ri-text">
-      <template v-for="(p,index) in properties"  :key="index">
-        <div  v-if="properties.validator[index] !== undefined
+      <template v-for="(p,index) in properties" :key="index">
+        <div v-if="properties.validator[index] !== undefined
                 && properties.validator[index].type === 'String' && index !== 'name'">
           <label class="input-container">
-            {{index}}
+            {{ index }}
             <input type="text" v-model="properties[index]" :pattern="properties.validator[index].regex">
           </label>
         </div>
       </template>
     </collapsible>
-    <collapsible v-if="properties.width !== undefined || properties.height !== undefined"  title="Size control" icon="ri-pencil-ruler-2-line">
+    <collapsible v-if="properties.width !== undefined || properties.height !== undefined" title="Size control"
+                 icon="ri-pencil-ruler-2-line">
       <label class="input-container" v-if="properties.width !== undefined">
-        Width <template v-if="isLinkedWidthHeight"> & height </template> :
-        <dinput  :on-update="widthUpdate" v-model="properties.width" min-value="0" max-value="999" :percentable="true" />
+        Width
+        <template v-if="isLinkedWidthHeight"> & height</template>
+        :
+        <dinput :on-update="widthUpdate" v-model="properties.width" min-value="0" max-value="999" :percentable="true"/>
       </label>
-      <label class="input-container" v-if="properties.height !== undefined && !isLinkedWidthHeight" >
+      <label class="input-container" v-if="properties.height !== undefined && !isLinkedWidthHeight">
         Height:
-        <dinput v-model="properties.height" min-value="0" max-value="999" :percentable="true" />
+        <dinput v-model="properties.height" min-value="0" max-value="999" :percentable="true"/>
       </label>
       <div v-if="properties.width !== undefined && properties.height !== undefined">
         <toggle icon="ri-links-line" label="Link width & height" v-model="isLinkedWidthHeight" :size=".6"></toggle>
       </div>
     </collapsible>
     <collapsible title="Colors properties" icon="ri-palette-line">
-      <template v-for="(p,index) in properties"  :key="index">
-        <div  v-if="properties.validator[index] !== undefined
+      <template v-for="(p,index) in properties" :key="index">
+        <div v-if="properties.validator[index] !== undefined
                 && properties.validator[index].type === 'String|Color'">
           <color-picker v-model="properties[index]" :label="index"></color-picker>
         </div>
@@ -60,30 +64,38 @@
       <around v-model="properties.padding"></around>
     </collapsible>
     <collapsible title="Checkable properties" icon="ri-checkbox-multiple-line">
-      <template v-for="(p,index) in properties"  :key="index">
-        <div  v-if="properties.validator[index] !== undefined
+      <template v-for="(p,index) in properties" :key="index">
+        <div v-if="properties.validator[index] !== undefined
                 && properties.validator[index].type === 'Boolean'">
           <toggle :label="index" v-model="properties[index]" :size=".6"></toggle>
         </div>
       </template>
     </collapsible>
     <collapsible v-if="properties.actions !== undefined" title="Actions" icon="ri-stack-line">
-        <div v-for="(act,i) in properties.actions" :key="i">
-            {{act}}
-        </div>
-        New action:
-        <div>
-          <div class="input-container">
-            <input type="text" ref="tooltip" v-model="tooltip" placeholder="Tool tip">
-          </div>
-          <icon-picker label="Action icon" v-model="actIcon"></icon-picker>
 
-          <div class="text-center">
-            <button class="circle-btn small m-auto" @click="addAction">
-              <i class="ri-add-line"></i>
-            </button>
-          </div>
+      <ul>
+        <li v-for="(act,i) in properties.actions" :key="i">
+          <icon-picker :label="act.tooltip" v-model="properties.actions[i].icon">
+            <div class="code-editor">
+              <i class="ri-code-s-slash-line"></i>
+            </div>
+          </icon-picker>
+        </li>
+      </ul>
+      <hr>
+      New action:
+      <div>
+        <div class="input-container">
+          <input type="text" ref="tooltip" v-model="tooltip" placeholder="Tool tip">
         </div>
+        <icon-picker label="Action icon" v-model="actIcon"></icon-picker>
+
+        <div class="text-center">
+          <button class="circle-btn small m-auto" @click="addAction">
+            <i class="ri-add-line"></i>
+          </button>
+        </div>
+      </div>
     </collapsible>
 
   </div>
@@ -92,13 +104,14 @@
 <script>
 import {mapGetters, mapState} from 'vuex';
 import collapsible from "./collapsible.vue";
-import  toggle from './switch.vue';
+import toggle from './switch.vue';
 import around from './around-controller.vue'
 import colorPicker from './color-picker.vue'
 import dinput from './input-draggable.vue';
 import iconPicker from './icon-picker.vue';
 import {useToast} from "vue-toastification";
-let toast ;
+
+let toast;
 export default {
   name: "modern-properties",
   components: {
@@ -113,7 +126,7 @@ export default {
     return {
       isNameValid: true,
       isLinkedWidthHeight: false,
-      test:'',
+      test: '',
       tooltip: '',
       actIcon: '',
     }
@@ -127,89 +140,108 @@ export default {
     ...mapState('ide', {
       properties: 'onEditComponent',
     }),
-    ...mapGetters('project',['appColor'])
+    ...mapGetters('project', ['appColor'])
   },
   methods: {
     /**
      * extract items form regex
      * @param regex
      */
-    extractItems(regex){
+    extractItems(regex) {
       try {
-        return  regex.split('(')[1].split(')')[0].split('|');
+        return regex.split('(')[1].split(')')[0].split('|');
       } catch {
-          return [];
+        return [];
       }
 
     },
-    showValue(title){
-      if (title === 'null'){
+    showValue(title) {
+      if (title === 'null') {
         return 'Default';
       }
-      return  title;
+      return title;
     },
-    checkName(){
+    checkName() {
       if (this.properties.name.match(this.properties.validator.name.regex)) {
         this.isNameValid = true;
-      }else{
+      } else {
         this.isNameValid = false;
       }
       // need to check other names of project
     },
-    canBlur(e){
-      if (e.target.getAttribute('data-blur') === 'false'){
+    canBlur(e) {
+      if (e.target.getAttribute('data-blur') === 'false') {
         e.target.focus();
       }
     },
-    widthUpdate(){
-      if (this.isLinkedWidthHeight){
+    widthUpdate() {
+      if (this.isLinkedWidthHeight) {
         this.properties.height = this.properties.width;
       }
     },
-    addAction(){
+    addAction() {
       console.log('action add');
-      if (this.tooltip === ''){
+      if (this.tooltip === '') {
         this.$refs.tooltip.focus();
         return false;
       }
-      if (this.actIcon === ''){
+      if (this.actIcon === '') {
         toast.warning('Icon is necessary!');
         return false;
       }
       this.properties.actions.push({
-        icon: this.icon,
+        icon: this.actIcon,
         onPressed: "// run when press on action ",
         tooltip: this.tooltip,
       });
+      this.tooltip = '';
     }
   }
 }
 </script>
 
 <style scoped>
-  input{
-    padding: 4px;
-  }
-  .prop-container{
-    overflow-y: auto;
-  }
-  .err{
-    background: #8B000066;
-    padding: 4px;
-  }
-  .btn-group{
-    display: grid;
-    grid-auto-columns: minmax(0, 1fr);
-    grid-auto-flow: column;
-    padding: .5rem .2rem;
-  }
-  .btn-group .btn{
-    border: 1px solid var(--lighter-bg);
-    text-align: center;
-    transition: var(--transition-duration);
-  }
-  .btn-group .btn.active{
-    color: var(--text-hilight);
-    border-color: var(--text-hilight-darker);
-  }
+input {
+  padding: 4px;
+}
+
+.prop-container {
+  overflow-y: auto;
+}
+
+.err {
+  background: #8B000066;
+  padding: 4px;
+}
+
+.btn-group {
+  display: grid;
+  grid-auto-columns: minmax(0, 1fr);
+  grid-auto-flow: column;
+  padding: .5rem .2rem;
+}
+
+.btn-group .btn {
+  border: 1px solid var(--lighter-bg);
+  text-align: center;
+  transition: var(--transition-duration);
+}
+
+.btn-group .btn.active {
+  color: var(--text-hilight);
+  border-color: var(--text-hilight-darker);
+}
+
+.code-editor {
+  background: var(--def-bg);
+  display: flex;
+  width: 2em;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  transition: var(--transition-duration);
+}
+.code-editor:hover{
+  background: var(--text-hilight);
+}
 </style>
