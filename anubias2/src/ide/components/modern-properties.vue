ea24ac7c873f7016941bd105c0647eddd7c93e1e
A1Gard <a1gard@4xmen.ir>
2023-06-06 03:47:20 +0330

    fixed bug to show empty props fixed input style

--- anubias2/src/ide/components/modern-properties.vue
+++ anubias2/src/ide/components/modern-properties.vue
@@ -25,7 +25,7 @@
         </div>
       </div>
     </collapsible>
-    <collapsible title="Text properties" icon="ri-text">
+    <collapsible v-if="propTypeCount('String') > 0" icon="ri-text" title="Text properties">
       <template v-for="(p,index) in properties" :key="index">
         <div v-if="properties.validator[index] !== undefined
                 && properties.validator[index].type === 'String' && index !== 'name'">
@@ -41,7 +41,7 @@
         </div>
       </template>
     </collapsible>
-    <collapsible icon="ri-bank-card-line" title="Choose properties">
+    <collapsible v-if="propTypeCount('String|select') > 0" icon="ri-bank-card-line" title="Choose properties">
       <template v-for="(p,index) in properties" :key="index">
         <div v-if="properties.validator[index] !== undefined
                 && properties.validator[index].type === 'String|Select'">
@@ -55,18 +55,23 @@
         </div>
       </template>
     </collapsible>
-    <collapsible v-if="properties.width !== undefined || properties.height !== undefined" title="Number control"
-                 icon="ri-pencil-ruler-2-line">
-      <template v-for="(p,index) in properties" :key="index">
-        <div v-if="properties.validator[index] !== undefined
+    <div v-if="propTypeCount('Number') > 0">
+
+      <collapsible v-if="properties.width !== undefined || properties.height !== undefined"
+                   icon="ri-pencil-ruler-2-line"
+                   title="Number control">
+        <template v-for="(p,index) in properties" :key="index">
+          <div v-if="properties.validator[index] !== undefined
                 && properties.validator[index].type === 'Number' && index !== 'width' && index !== 'height'">
-          <label v-if="properties.height !== undefined && !isLinkedWidthHeight" class="input-container">
-            {{ index }}:
-            <dinput v-model="properties[index]" :percentable="true" max-value="9999999" min-value="0"/>
-          </label>
-        </div>
-      </template>
-    </collapsible>
+            <label v-if="properties.height !== undefined && !isLinkedWidthHeight" class="input-container">
+              {{ index }}:
+              <dinput v-model="properties[index]" :percentable="true" max-value="9999999" min-value="0"/>
+            </label>
+          </div>
+        </template>
+      </collapsible>
+    </div>
+
     <collapsible v-if="properties.width !== undefined || properties.height !== undefined" icon="ri-pencil-ruler-2-line"
                  title="Size control">
       <label class="input-container" v-if="properties.width !== undefined">
@@ -83,7 +88,7 @@
         <toggle icon="ri-links-line" label="Link width & height" v-model="isLinkedWidthHeight" :size=".6"></toggle>
       </div>
     </collapsible>
-    <collapsible title="Colors properties" icon="ri-palette-line">
+    <collapsible v-if="propTypeCount('String|Color') > 0" icon="ri-palette-line" title="Colors properties">
       <template v-for="(p,index) in properties" :key="index">
         <div v-if="properties.validator[index] !== undefined
                 && properties.validator[index].type === 'String|Color'">
@@ -94,7 +99,7 @@
     <collapsible v-if="properties.padding !== undefined" title="Padding">
       <around v-model="properties.padding"></around>
     </collapsible>
-    <collapsible title="Checkable properties" icon="ri-checkbox-multiple-line">
+    <collapsible v-if="propTypeCount('Boolean') > 0" icon="ri-checkbox-multiple-line" title="Checkable properties">
       <template v-for="(p,index) in properties" :key="index">
         <div v-if="properties.validator[index] !== undefined
                 && properties.validator[index].type === 'Boolean'">
@@ -208,6 +213,38 @@ export default {
         return [];
       }
 
+    },
+    propTypeCount(type) {
+      if (this.properties === null) {
+        return 0;
+      }
+      let count = 0;
+      const except = ['type',
+        'width',
+        'height',
+        'name',
+        'validator',
+        'margin',
+        'padding',
+      ];
+
+      try {
+        for (let [i] of Object.entries(this.properties)) {
+
+          if (except.indexOf(i) !== -1) {
+            continue;
+          }
+          let p = this.properties.validator[i];
+          if (p.type === type) {
+            count++;
+          }
+        }
+      } catch (e) {
+        console.log(e.message);
+        return 0;
+      }
+
+      return count;
     },
     showValue(title) {
       if (title === 'null') {
